948a0360f6782a068426b24d2770a8f64228aa17
Ramyasowjanya <ramyasowjanya.s@gmail.com>
2016-10-13 00:41:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,8 @@ var heading=document.getElementById("text");
 
 heading.innerHTML="This is new hading";
 
-var image=document.getElementById("madi");
-image.onClick=function()
+var img=document.getElementById("madi");
+img.onClick=function()
 {
-    image.style.marginLeft='15px';
+    img.style.marginLeft='15px';
 };
